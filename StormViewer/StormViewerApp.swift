6fc71006ee4da70a41d0060edc767f509d68c500
MacBook Air <macbookair@ringgit.me>
2024-06-27 16:55:33 +0800

    Initial Commit

---
+++ StormViewer/StormViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StormViewerApp.swift
+//  StormViewer
+//
+//  Created by Long Fong Yee on 27/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct StormViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
